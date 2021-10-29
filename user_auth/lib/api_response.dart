268dae71aaec44c572558edb5abdd729c81299ed
Pravin Pawar <pravinvpawar5551@gmail.com>
2021-10-29 09:12:12 +0530

    api_response.dart created

--- user_auth/lib/api_response.dart
+++ user_auth/lib/api_response.dart
@@ -8,6 +8,7 @@ class ApiResponse {
   Object get Data => _data;
   set Data(Object data) => _data = data;
 
+
   Object get ApiError => _apiError as Object;
   set ApiError(Object error) => _apiError = error;
 }
